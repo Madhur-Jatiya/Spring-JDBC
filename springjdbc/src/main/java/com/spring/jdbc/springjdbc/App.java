52f517019ba2e4b8f68673b8a294be065c62c066
Madhurmoms <madhurjatiya13@gmail.com>
2023-01-29 22:06:55 +0530

    new project

---
+++ springjdbc/src/main/java/com/spring/jdbc/springjdbc/App.java
@@ -0,0 +1,13 @@
+package com.spring.jdbc.springjdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
