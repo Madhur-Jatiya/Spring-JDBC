80dcb1f70cc90f1d83b398732a0955c205e49b71
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-03 00:23:36 +0530

    create student dao

--- springjdbc/src/main/java/com/spring/jdbc/dao/StudentDao.java
+++ springjdbc/src/main/java/com/spring/jdbc/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.spring.jdbc.dao;
 
+import com.spring.jdbc.entities.Student;
+
 public interface StudentDao {
-	public int insert();	
+	public int insert(Student student);	
 }
--- springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ springjdbc/src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -1,10 +1,24 @@
 package com.spring.jdbc.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.entities.Student;
+
 public class StudentDaoImpl implements StudentDao{
 
-	public int insert() {
-		// TODO Auto-generated method stub
+	private JdbcTemplate template;
+	
+	public int insert(Student student) {
 		return 0;
 	}
+
+	public JdbcTemplate getTemplate() {
+		return template;
+	}
+
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
+
 	
 }
